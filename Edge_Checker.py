cdb36a72da842efff4216c1fc6d722878291ce35
Velidi Lokesh Naidu <82692724+lokeshvelidi@users.noreply.github.com>
2022-10-13 11:15:52 +0530

    a,b=map(int,input().split()) if b==a+1 or b==a-1 or (a==10 and b==1) or (a==1 and b==10):     print('Yes') else:     print('No')

--- Edge_Checker.py
+++ Edge_Checker.py
@@ -1,5 +1,5 @@
 a,b=map(int,input().split())
-if b-a==9 or b-a==1 or b-a==-1 or b-a==-9:
-    print("Yes")
+if b==a+1 or b==a-1 or (a==10 and b==1) or (a==1 and b==10):
+    print('Yes')
 else:
-    print("No")
+    print('No')
