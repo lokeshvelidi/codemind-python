f6914606dc85d144a5ad45b10f3958b9f2b52ef0
Velidi Lokesh Naidu <82692724+lokeshvelidi@users.noreply.github.com>
2022-10-14 11:37:49 +0530

    b=int(input()) a=b while True:     pr=True     for i in range(2,int(a**0.5)+1):         if a%i==0:             pr=False             break     if pr == True:         break     else:         a+=1 c=b while True:     pr=True     for i in range(2,int(c**0.5)+1):         if c%i==0:             pr=False             break     if pr == True:         break     else:         c-=1 d=b-c e=a-b if e>=d:     print(d) else:     print(e)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,29 @@
+b=int(input())
+a=b
+while True:
+    pr=True
+    for i in range(2,int(a**0.5)+1):
+        if a%i==0:
+            pr=False
+            break
+    if pr == True:
+        break
+    else:
+        a+=1
+c=b
+while True:
+    pr=True
+    for i in range(2,int(c**0.5)+1):
+        if c%i==0:
+            pr=False
+            break
+    if pr == True:
+        break
+    else:
+        c-=1
+d=b-c
+e=a-b
+if e>=d:
+    print(d)
+else:
+    print(e)
